18e0cca36bdbd536485b16030bf3b01665570199
thomas bartier <thomas.bartier@gmail.com>
2025-11-06 16:09:30 +0100

    add title & props filter games

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,15 +5,22 @@ import { GamesMockList } from "@/helpers/GamesMockList";
 
 const HomePage = () => {
   const list = GamesMockList
+
+    //   const date = new Date();
+    //   const year = date.getFullYear();
+  
+    // const [searchTerm, setSearchTerm] = useState("");
+    // const [selectedYear, setSelectedYear] = useState<string>(year.toString());
+    // const [selectedConsole, setSelectedConsole] = useState<string>("all");
+
   return (
     <div>
       <CustomNavbar />
       <div className="contenu">
         <div className="mt-5">
-          <FiltersGames />
+          <FiltersGames games={list.length}/>
         </div>
 
-{/* TODO mettre titre + nombre de jeux dans la list => Mes jeux (de 2025 (5) ) */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {list.map((game) => (
             <CardGame key={game.id} game={game} />
