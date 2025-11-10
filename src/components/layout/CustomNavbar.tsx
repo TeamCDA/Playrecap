f6ac8afadfef3306dea1db0b4bb2e616ea62ac86
thomas bartier <thomas.bartier@gmail.com>
2025-11-10 13:33:13 +0100

    update navbar responsive + color & translate navlink

--- src/components/layout/CustomNavbar.tsx
+++ src/components/layout/CustomNavbar.tsx
@@ -1,13 +1,15 @@
 import { Gamepad2, User2 } from "lucide-react";
 import { NavLink } from "react-router-dom";
 import { ModeToggle } from "../mode-toggle";
+import { useTranslation } from "react-i18next";
 
 const CustomNavbar = () => {
+  const { t } = useTranslation();
 
   return (
     <div className="bg-muted">
       <nav className="h-16 bg-background border-b">
-        <div className="h-full flex items-center justify-between max-w-7xl mx-auto px-3 sm:px-6 lg:px-8">
+        <div className="h-full flex items-center justify-between max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
           {/* Logo */}
           <div className="flex items-center gap-2 justify-center">
             <Gamepad2 size={30} />
@@ -20,29 +22,29 @@ const CustomNavbar = () => {
             <NavLink
               to="/home"
               className={({ isActive }) =>
-                `font-semibold flex items-center gap-2 px-3 py-2 rounded-md transition-colors ${
+                `font-semibold flex items-center gap-2 max-sm:gap-1 px-3 py-2 rounded-md transition-colors ${
                   isActive
-                    ? "bg-primary text-white hover:bg-orange-600"
+                    ? "bg-primary text-white hover:bg-primary/90"
                     : "hover:bg-accent"
                 }`
               }
             >
               <Gamepad2 size={18} />
-              <p>Jeux</p>
+              <p className="">{t("NAVBAR.GAMES")}</p>
             </NavLink>
 
             <NavLink
               to="/profile"
               className={({ isActive }) =>
-                `font-semibold flex items-center gap-2 px-3 py-2 rounded-md transition-colors ${
+                `font-semibold flex items-center gap-2 max-sm:gap-1 px-3 py-2 rounded-md transition-colors ${
                   isActive
-                    ? "bg-primary text-white hover:bg-orange-600"
+                    ? "bg-primary text-white hover:bg-primary/90"
                     : "hover:bg-accent"
                 }`
               }
             >
               <User2 size={18} />
-              <p>Profil</p>
+              <p className="">{t("NAVBAR.PROFILE")}</p>
             </NavLink>
           </div>
         </div>
